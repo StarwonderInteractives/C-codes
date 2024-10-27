3aeedee3e193698dfdcd5f4ce227564a592bdc6e
Starwonder Interactives <hasanabbaskazim@gmail.com>
2024-10-27 12:42:29 +0530

    Add files via upload

---
+++ BILLING MACHINE.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class BILLING_MACHINE
+{
+    static void Main()
+    {
+        int[] cod = new int[5];
+        int[] price = new int[5];
+        int[] qty = new int[5];
+        int[] tc = new int[5];
+        int s1 = 0, s2 = 0, s3 = 0;
+
+        Console.WriteLine("ENTER CODE, PRICE AND QUANTITY");
+
+        for (int i = 0; i < 5; i++)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            cod[i] = int.Parse(input[0]);
+            price[i] = int.Parse(input[1]);
+            qty[i] = int.Parse(input[2]);
+            s1 += price[i];
+            s2 += qty[i];
+            tc[i] = price[i] * qty[i];
+            s3 += tc[i];
+        }
+
+        Console.WriteLine("CODE\tPRICE\tQTY\tTOTAL");
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{cod[i]}\t{price[i]}\t{qty[i]}\t{tc[i]}");
+        }
+
+        Console.WriteLine("SUM OF PRICE: " + s1);
+        Console.WriteLine("TOTAL ITEMS: " + s2);
+        Console.WriteLine("SUM OF TOTAL: " + s3);
+    }
+}
+
---
+++ CALCULATOR.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Calculator
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to the Calculator made in C#");
+
+        double n1, n2;
+        Console.WriteLine("Enter 1st number");
+        n1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter 2nd number");
+        n2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("A to add");
+        Console.WriteLine("S to subtract");
+        Console.WriteLine("M to multiply");
+        Console.WriteLine("D to divide");
+
+        char ch = Convert.ToChar(Console.ReadLine().ToUpper());
+
+        switch (ch)
+        {
+            case 'A':
+                Console.WriteLine(n1 + n2);
+                break;
+
+            case 'S':
+                Console.WriteLine(n1 - n2);
+                break;
+
+            case 'M':
+                Console.WriteLine(n1 * n2);
+                break;
+
+            case 'D':
+                if (n2 != 0)
+                    Console.WriteLine(n1 / n2);
+                else
+                    Console.WriteLine("Division by zero is not allowed.");
+                break;
+
+            default:
+                Console.WriteLine("ERROR");
+                break;
+        }
+    }
+}
